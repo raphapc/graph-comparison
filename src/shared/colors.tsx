55d821f0c321ad2c7b7f7daf76b014ba47ed2342
Raphael Pontes Campos <furacker@gmail.com>
2023-12-14 22:29:58 +0100

    Added tailwind colors

--- src/shared/colors.tsx
+++ src/shared/colors.tsx
@@ -1,10 +1,21 @@
 export const getColors = () => [
-  "#F94144",
-  "#F3722C",
-  "#F8961E",
-  "#F9C74F",
-  "#90BE6D",
-  "#43AA8B",
-  "#277DA1",
-  "#CCCCCC",
+  "#ef4444",
+  "#f97316",
+  "#eab308",
+  "#22c55e",
+  "#3b82f6",
+  "#6366f1",
+  "#a855f7",
+  "#ec4899",
+];
+
+export const colorsTailwind = [
+  "red-500",
+  "orange-500",
+  "yellow-500",
+  "green-500",
+  "blue-500",
+  "indigo-500",
+  "purple-500",
+  "pink-500",
 ];
